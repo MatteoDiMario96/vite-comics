592e4bedcf691937db816c2bebda3eb27b528b14
MatteoDiMario96 <dm.matteo96@live.it>
2024-06-10 23:57:01 +0200

    Fontawesome npm Aggiunti i favicon.

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -4,15 +4,41 @@ export default {
         return {
             
         }
-    } 
+    },
+    methods:{
+
+    },
+    created(){
+
+    }
 }
 </script>
 
 
 <template>
-    <h1>
-        Sono il Footer
-    </h1>
+    <footer>
+        <section id="ul-links">
+            <button>
+                Signup-Now
+            </button>
+        </section>
+
+        <section id="sign-up-social">
+            <div id="follow_us">
+                <h2>
+                Follow us
+                </h2>
+            </div>
+            <div id="favicon">
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-twitter"></i>
+                <i class="fa-brands fa-youtube"></i>
+                <i class="fa-brands fa-pinterest-p"></i>
+                <i class="fa-brands fa-google"></i>
+            </div>
+
+        </section>
+    </footer>
 </template>
 
 <style scoped>
