d843b1a89f0656d73381946818b3dde65984e1fb
MatteoDiMario96 <dm.matteo96@live.it>
2024-06-10 23:06:21 +0200

    Rinominata cartella. img2 Creato array oggetti UlHeaderLinks per la lista di link header. creato array oggetto per l'iimagine dinamica. Stilizzato un minimo. Impaginato un minimo.

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -2,7 +2,71 @@
 export default {
     data() {
         return {
-            
+            UlHeaderLinks: [
+                {   
+                    id: 1,
+                    title: "CHARACTERS",
+                    active: false,
+                    href: "#",
+                },{ 
+                    id: 2,
+                    title: "COMICS",
+                    active: true,
+                    href: "#",
+                },{ 
+                    id: 3,
+                    title: "MOVIES",
+                    active: false,
+                    href: "#",
+                },{ 
+                },{ 
+                    id: 4,
+                    title: "TV",
+                    active: false,
+                    href: "#",
+                },{ 
+                },{ 
+                    id: 5,
+                    title: "GAMES",
+                    active: false,
+                    href: "#",
+                },{ 
+                },{ 
+                    id: 6,
+                    title: "COLLECTIBLES",
+                    active: false,
+                    href: "#",
+                },{ 
+                },{ 
+                    id: 7,
+                    title: "VIDEOS",
+                    active: false,
+                    href: "#",
+                },{ 
+                },{ 
+                    id: 8,
+                    title: "FANS",
+                    active: false,
+                    href: "#",
+                },{ 
+                },{ 
+                    id: 9,
+                    title: "NEWS",
+                    active: false,
+                    href: "#",
+                },{ 
+                },{ 
+                    id: 10,
+                    title: "SHOP",
+                    active: false,
+                    href: "#",
+                },
+            ],
+            MainLogo:{
+                src: './src/assets/img2/dc-logo.png',
+                href: '#',
+                alt: "Main logo Dc Comics",
+            }
         }
     },
     methods:{
@@ -15,11 +79,41 @@ export default {
 </script>
 
 <template>
-    <h1>
-        Sono l'header
-    </h1>
+    <header>
+        <nav>
+            <div id="div-logo">
+                <a :href="MainLogo.href">
+                    <img :src="MainLogo.src" :alt="MainLogo.alt" id="img-logo">
+                </a>
+            </div>
+
+            <div id="ul-header">
+                <ul>
+                    <li v-for="link in UlHeaderLinks" :key="link.id">
+                        <a :href="link.href">
+                            {{ link.title }}
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+    @use '../styles/partials/variables' as *;
 
+    header{
+        height: 90px;
+
+    }
+    ul{
+        display: flex;
+        li{
+            padding: 1rem;
+            a{
+                color: $primary-color
+            }
+        }
+    }
 </style>
